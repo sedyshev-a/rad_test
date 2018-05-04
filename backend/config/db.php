a11d976060b444d054883c51c3b7b2c0eb06aab1
Andrey Sedyshev <sedyshev.a@gmail.com>
2018-05-04 11:23:12 +0300

    init commit

---
+++ backend/config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=rad_test',
+    'username' => 'developer',
+    'password' => 'super-password',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
